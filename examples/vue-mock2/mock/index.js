5f2a8983bd3def63aeaeccca61d33d891d31adac
Xu Liangzhan <xlz26296x@163.com>
2018-02-27 23:38:29 +0800

    update 1.5.2

--- examples/vue-mock2/mock/index.js
+++ examples/vue-mock2/mock/index.js
@@ -7,15 +7,4 @@ define([
 ], function (XEAjax, XEAjaxMock) {
   // 安装
   XEAjax.use(XEAjaxMock)
-
-  // 设置全局参数
-  XEAjaxMock.setup({
-    timeout: '100-1000', // 服务请求时间范围
-    // 设置默认响应头
-    headers: {
-      'Content-Type': 'application/javascript; charset=UTF-8'
-    },
-    error: true, // 控制台打印 Mock Error 日志
-    log: true // 控制台打印 Mock Request 日志
-  })
 })
