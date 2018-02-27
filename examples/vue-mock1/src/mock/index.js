e49c4287e27b714cee39872c6b7a381cf2da928b
Xu Liangzhan <xlz26296x@163.com>
2018-02-27 23:37:56 +0800

    update 1.5.2

--- examples/vue-mock1/src/mock/index.js
+++ examples/vue-mock1/src/mock/index.js
@@ -1,21 +1,10 @@
 import XEAjax from 'xe-ajax'
 import XEAjaxMock from 'xe-ajax-mock'
 
+// 导入虚拟服务配置
+import './json/api/shopping'
+import './json/api/user'
+import './jsonp/xuliangzhan.com'
+
 // 安装
 XEAjax.use(XEAjaxMock)
-
-// 设置全局参数
-XEAjaxMock.setup({
-  timeout: '100-1000', // 服务请求时间范围
-  // 设置默认响应头
-  headers: {
-    'Content-Type': 'application/javascript; charset=UTF-8'
-  },
-  error: true, // 控制台打印 Mock Error 日志
-  log: true // 控制台打印 Mock Request 日志
-})
-
-// 导入虚拟服务配置
-require('./json/api/shopping')
-require('./json/api/user')
-require('./jsonp/xuliangzhan.com')
