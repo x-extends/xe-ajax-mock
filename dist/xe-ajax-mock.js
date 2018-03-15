7c058b53984e63a7cb2bd0cdc994eb1abcf3266a
Xu Liangzhan <xu_liangzhan@163.com>
2018-03-15 20:26:26 +0800

    update 1.6.6

--- dist/xe-ajax-mock.js
+++ dist/xe-ajax-mock.js
@@ -507,38 +507,40 @@
   /**
    * jsonp
    */
-  function xejsonp (script, request, resolve, reject) {
-    var mockItem = mateMockItem(request)
-    $global[request.jsonpCallback] = function (body) {
-      resolve({ status: 200, body: body })
-    }
-    if (mockItem) {
-      mockItem.time = getScopeNumber(mockItem.options.timeout)
-      return mockItem.getMockResponse(mockItem, request).then(function (response) {
-        if (response.status < 200 || response.status >= 300) {
-          script.onerror({ type: 'error' })
-        } else {
-          $global[request.jsonpCallback](response.body)
+  function xejsonp (script, request) {
+    return new Promise(function (resolve) {
+      var mockItem = mateMockItem(request)
+      $global[request.jsonpCallback] = function (body) {
+        resolve({ status: 200, body: body })
+      }
+      if (mockItem) {
+        mockItem.time = getScopeNumber(mockItem.options.timeout)
+        return mockItem.getMockResponse(mockItem, request).then(function (response) {
+          if (response.status < 200 || response.status >= 300) {
+            script.onerror({ type: 'error' })
+          } else {
+            $global[request.jsonpCallback](response.body)
+          }
+          mockItem.outMockLog(request, response)
+        })
+      } else {
+        var url = request.getUrl()
+        script.type = 'text/javascript'
+        script.src = url + (url.indexOf('?') === -1 ? '?' : '&') + request.jsonp + '=' + request.jsonpCallback
+        script.onerror = function (evnt) {
+          resolve({ status: 500, body: '' })
         }
-        mockItem.outMockLog(mockItem, request, response)
-      })
-    } else {
-      var url = request.getUrl()
-      script.type = 'text/javascript'
-      script.src = url + (url.indexOf('?') === -1 ? '?' : '&') + request.jsonp + '=' + request.jsonpCallback
-      script.onerror = function (evnt) {
-        resolve({ status: 500, body: '' })
+        script.onabort = function (evnt) {
+          resolve({ status: 0, body: '' })
+        }
+        if (request.timeout) {
+          setTimeout(function () {
+            script.onabort()
+          }, request.timeout)
+        }
+        document.body.appendChild(script)
       }
-      script.onabort = function (evnt) {
-        resolve({ status: 0, body: '' })
-      }
-      if (request.timeout) {
-        setTimeout(function () {
-          script.onabort()
-        }, request.timeout)
-      }
-      document.body.appendChild(script)
-    }
+    })
   }
 
   var requireMap = {}
